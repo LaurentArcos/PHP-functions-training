175f3c9f7bc5e4edd4ec335f390c1cc31413ac8e
LaurentArcos <laurent.arcos@gmail.com>
2023-04-04 15:43:46 +0200

    fin des exercices fonctions natives PHP

--- censure.php
+++ censure.php
@@ -1,11 +1,28 @@
 <?php
 
 $insultes = ['gredin', 'tagazou', 'gougnafier', 'mastre'];
+$asterisque = [];
+
+foreach ($insultes as $insulte){
+
+  $asterisque[] = substr($insulte,0,1) . str_repeat('*', strlen($insulte) -1);
+
+} 
 
 $phrase = readline("Entrez une phrase : ");
 
-foreach($insultes as $insulte) {
-  $phrase = str_replace($insulte, '******', $phrase);
-}
+$phrase = str_replace($insultes, $asterisque, $phrase);
 
 echo $phrase;
+
+
+// $insultes = ['gredin', 'tagazou', 'gougnafier', 'mastre'];
+
+// $phrase = readline("Entrez une phrase : ");
+
+// foreach($insultes as $insulte) {
+//   $replace = str_repeat('*', strlen($insulte));
+//   $phrase = str_replace($insulte, $replace, $phrase);
+// }
+
+// echo $phrase;
