34d77c9b115ecfc8817187a708470a8a20baaa3e
LaurentArcos <laurent.arcos@gmail.com>
2023-04-04 15:06:51 +0200

    calcul Moyenne exercice ok

--- calculMoyenne.php
+++ calculMoyenne.php
@@ -1,1 +1,9 @@
 <?php
+$notes = [10,12,25,30];
+$notes[] = 16;
+array_push($notes, 3, 5, 18);
+$sum = array_sum($notes);
+$count = count($notes);
+echo "Vous avez " . round($sum / $count, 2) . " de moyenne";
+
+
