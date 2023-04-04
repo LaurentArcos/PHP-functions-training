0532d3e5e65d097eabd6a0fa3fea13f4c9db7949
LaurentArcos <laurent.arcos@gmail.com>
2023-04-04 15:21:52 +0200

    utilisation de str_replace

--- censure.php
+++ censure.php
@@ -1,1 +1,11 @@
 <?php
+
+$insultes = ['gredin', 'tagazou', 'gougnafier', 'mastre'];
+
+$phrase = readline("Entrez une phrase : ");
+
+foreach($insultes as $insulte) {
+  $phrase = str_replace($insulte, '******', $phrase);
+}
+
+echo $phrase;
